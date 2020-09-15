cce00b1fd891036c68d373a0080700eb64348b67
Alex <mr.rozlivanov@gmail.com>
2020-09-15 12:23:49 +0300

    Fixed prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  api_login_base_url: 'https://api.dribbble.com/v2',
+  client_id: '9b48cbd8edfb43c12b523ff073eed1c794016588adc941aab47f4284f08536ce',
+  client_secret: 'df6d126d9dbda60fe2867d01a7fa5cc86842f3aab82a7ba645f0a3151e717b0a',
+  host: 'http://localhost:4200'
 };
